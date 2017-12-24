c9143695aa6319b94f639ff8aa5dbc24e1ec3052
Kriston Costa <kristoncosta@gmail.com>
2017-12-24 16:05:28 -0500

    Removing old 06

--- projects/06/Assembler/CommandType.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-enum CommandType
-{
-	A_COMMAND = 1,
-	C_COMMAND = 2,
-	L_COMMAND = 3
-};
--- projects/06/Assembler/DestinationTypes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-enum Destination {
-	A_DEST = 1,
-	M_DEST = 2,
-	D_DEST = 3	
-};
